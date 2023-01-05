66ae7898b8f34e5f8e427de0b1d30627d9d3d2cc
stellarAlien <saifgaida@gmail.com>
2023-01-05 21:50:24 +0100

    removing clean from output

--- 0x01-Webpack/task_3/webpack.config.js
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, "public"),
-      clean: true,
+      //clean: true,
     },
     devServer: {
       static: {
