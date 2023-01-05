4574989d0170f75613a278858da8af598859f094
stellarAlien <saifgaida@gmail.com>
2023-01-05 16:47:42 +0100

    using debounce

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,19 +1,25 @@
-import $ from "jquery";
-import { debounce } from "lodash";
+const $ = require("jquery");
+import { debounce } from 'lodash';
 
 let count = 0;
 
-function update_counter(){
-    count ++;
-    $('#count').text(`${count} clicks on the button`);
+function updateCounter() {
+  count++;
+  $("#count").text(`${count} clicks on the button`);
 }
 
-const $button = $('#button').on(
-    'click',
-    debounce(update_counter, 500, {leading: true, trailing: false})
-    );
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append("<button id='button'></button>");
-$('body').append(`<p id='count'></p>`);
-$('body').append('<p>Copyright - Holberton School</p>');
+const $button = $("<button>Click here to get started</button>").on(
+  "click",
+  debounce(updateCounter, 500, {
+    leading: true,
+    trailing: false,
+  })
+);
+
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append($button);
+$("body").append("<p id='count'></p>");
+$("body").append("<p>Copyright - Holberton School</p>");
+
+
