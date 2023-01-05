94d9a9bc9fcce78d4de1b293cc4a16e5a1efc59b
stellarAlien <saifgaida@gmail.com>
2023-01-05 16:25:31 +0100

    fixed button?

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -8,12 +8,12 @@ function update_counter(){
     $('#count').text(`${count} clicks on the button`);
 }
 
-const $button = $('<button>Click here to get started</button>').on(
+const $button = $('#button').on(
     'click',
     debounce(update_counter, 500, {leading: true, trailing: false})
     );
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
-$('body').append($button);
+$('body').append("<button id='button'></button>");
 $('body').append(`<p id='count'></p>`);
 $('body').append('<p>Copyright - Holberton School</p>');
