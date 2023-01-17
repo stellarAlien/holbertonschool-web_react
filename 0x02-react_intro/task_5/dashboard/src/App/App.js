ae6b9bd6438e7ece247a7655cffd7c03d123c0b1
stellarAlien <saifgaida@gmail.com>
2023-01-17 18:15:00 +0100

    App body and footer css

--- 0x02-react_intro/task_5/dashboard/src/App/App.js
+++ 0x02-react_intro/task_5/dashboard/src/App/App.js
@@ -8,16 +8,16 @@ function App() {
       {/* Header */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {/* Body */}
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
         </a>
       </header>
            {/* Footer */}
