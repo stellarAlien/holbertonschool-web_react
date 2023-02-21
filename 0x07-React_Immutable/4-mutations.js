51ed563be96797df3a452666e5114eb02804ff48
stellarAlien <saifgaida@gmail.com>
2023-02-21 19:48:35 +0100

    eslint

--- 0x07-React_Immutable/4-mutations.js
+++ 0x07-React_Immutable/4-mutations.js
@@ -9,8 +9,8 @@ const map = Map({
   6: 'Lucas',
 });
 
-const map2 = map.withMutations(map => {
-    map.set(2, 'Benjamin').set(4, 'Oliver');
-  })
+const map2 = map.withMutations((mapItem) => {
+  mapItem.set(2, 'Benjamin').set(4, 'Oliver');
+});
 
 export { map, map2 };
