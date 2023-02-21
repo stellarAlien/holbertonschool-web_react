b81a3f983627f2afe1ac5351bccd057fc1882de3
stellarAlien <saifgaida@gmail.com>
2023-02-21 20:00:11 +0100

    check if nested field array has any value

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,6 +1,7 @@
 import { Map } from 'immutable';
 
 function accessImmutableObject(object, array) {
+  if (!array.length) { return undefined; }
   const immutableObject = Map(object);
   const value = immutableObject.getIn(array);
   return value !== undefined ? value : undefined;
