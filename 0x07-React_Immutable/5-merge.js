17ba1cd9c9770078e09b38a4bff4241d5726cfa3
stellarAlien <saifgaida@gmail.com>
2023-02-21 19:53:35 +0100

    return merged map only

--- 0x07-React_Immutable/5-merge.js
+++ 0x07-React_Immutable/5-merge.js
@@ -6,5 +6,5 @@ export function concatElements(page1, page2) {
 
 export function mergeElements(page1, page2) {
   const merged = Map(page1).merge(Map(page2));
-  return List(merged.values());
+  return merged;
 }
